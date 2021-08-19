9d9bea8bb473f5209f1e65a145e0f6eb8d3bdaf2
xingyuefeng <15262870437@163.com>
2021-08-19 15:41:26 +0800

    refactor: init code

---
+++ packages/CalendarManager/README.md
@@ -0,0 +1,11 @@
+# `CalendarManager`
+
+> TODO: description
+
+## Usage
+
+```
+const calendarManager = require('CalendarManager');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/CalendarManager/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function index() {
+  return (
+    <Text>
+      1111
+    </Text>
+  )
+}
